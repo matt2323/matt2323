cf319bd66cce7eb385f8271f37ec91296f1f5bab
Matt Frey <matthew.t.frey@gmail.com>
2016-12-31 13:18:57 -0500

    move to push notes

--- sw.js
+++ sw.js
@@ -1,67 +1,44 @@
-var CACHE_NAME = 'dependencies-cache';
+'use strict';
 
-// Files required to make this app work offline
-var REQUIRED_FILES = [
-  'content/heads.png',
-  'content/spin.gif',
-  'content/tails.png',
-  'manifest.json',
-  'website.css',
-  'index.html',
-  '/', // Separate URL than index.html!
-  'website.js'
-];
-
-
-self.addEventListener('install', function(event) {
-  // Perform install step:  loading each required file into cache
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // Add all offline dependencies to the cache
-        return cache.addAll(REQUIRED_FILES);
-      })
-      .then(function() {
-      	// At this point everything has been cached
-        return self.skipWaiting();
-      })
-  );
+console.log('Started', self);
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+    console.log('Installed', event);
 });
+self.addEventListener('activate', function (event) {
+    console.log('Activated', event);
+});
+self.addEventListener('push', function (event) {
+    console.log('Push message received', event);
+    var title = 'camera';
+    event.waitUntil(
+      self.registration.showNotification(title, {
+          body: 'alert',
+        //  icon: 'Content/runner.jpg',
+          tag: 'my-tag'
+      }));
+});
+self.addEventListener('notificationclick', function (event) {
+    console.log('On notification click: ', event.notification.tag);
+    // Android doesn't close the notification when you click on it  
+    // See: http://crbug.com/463146  
+    event.notification.close();
 
-self.addEventListener('fetch', function(event) {
-  
-  if (event.request.mode === 'navigate' && (event.request.url == "http://localhost/" ||  event.request.url == "https://matt2323.github.io/" ))
-  {
-      return fetch(event.request).catch(function(error) {
-        caches.match(event.request).then(function(response) {
-          // Cache hit - return the response from the cached version
-          if (response) {
-            return response;
+    // This looks to see if the current is already open and  
+    // focuses if it is  
+    event.waitUntil(
+      clients.matchAll({
+          type: "window"
+      })
+      .then(function (clientList) {
+          for (var i = 0; i < clientList.length; i++) {
+              var client = clientList[i];
+              if (client.url == '/' && 'focus' in client)
+                  return client.focus();
           }
-        });
-      }); 
-  }
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return the response from the cached version
-        if (response) {
-          return response;
-        }
-   
-        fetch(event.request).then(function(response) {
-          return caches.open(CACHE_NAME).then(function(cache) {
-            cache.put(event.request, response.clone());
-            return response;
-          });  
-      });
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
-  event.waitUntil(self.clients.claim());
-});
+          if (clients.openWindow) {
+              return clients.openWindow('/');
+          }
+      })
+    );
+})
